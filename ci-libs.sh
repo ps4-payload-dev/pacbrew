1f2cb1f8734b455e726d40fbeff40670772f2cab
John Törnblom <john.tornblom@gmail.com>
2025-08-08 21:06:23 +0200

    initial commit

---
+++ ci-libs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+PKGS=(sdk openlibm libcxx fast_float
+      bzip2 zlib xz zstd
+      libressl
+      libpsl
+      curl)
+
+sudo pacman --noconfirm --remove ps4-payload-dev
+
+for PKG in ${PKGS[*]} ; do
+    pushd $PKG || exit 1
+    rm -f *.pkg.tar.gz
+    rm -rf src pkg
+    makepkg -c -f || exit 1
+    sudo pacman --noconfirm -U ./ps4-payload-*.pkg.tar.gz || exit 1
+    popd
+done
